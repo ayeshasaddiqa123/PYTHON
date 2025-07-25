f75c49848ad1e6193aebf086fc918d3e2e1eda0d
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2025-07-25 17:21:11 +0500

    if

--- intermediate_level_of_Python.py
+++ intermediate_level_of_Python.py
@@ -93,5 +93,112 @@ print(d)
 
 
 
+----------------------------------------------------------------------------------------------------------
 
+# . Write a program to find the greatest of four numbers entered by the user. 
+
+a=int(input("Enter number :"))
+b=int(input("Enter number :"))
+c=int(input("Enter number :"))
+d=int(input("Enter number :"))
+
+if(a>b and a>c and a>d):
+    print(a , "is greater than all numbers.")
+elif(b>a and b>c and b>d):
+    print(b , "is greater than all numbers.")
+elif(c>a and c>b and c>d):
+    print(c , "is greater than all numbers.")
+else:
+    print(d , "is greater than all numbers.")    
+
+   
+# 2. Write a program to find out whether a student has passed or failed if it requires a 
+# total of 40% and at least 33% in each subject to pass. Assume 3 subjects and 
+# take marks as an input from the user. 
+
+
+sub1=int(input("Enter number :"))
+sub2=int(input("Enter number :"))
+sub3=int(input("Enter number :"))
+ 
+total_percentage=(100*(sub1+sub2+sub3))/300
+
+
+
+if(total_percentage>40  and sub1>33 and sub2>33 and sub3>33 ):
+    print("Congrats! you are passed" )
+
+else:
+    print("oops! you are failed")
+# 3. A spam comment is defined as a text containing following keywords: 
+# “Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program 
+# to detect these spams.
+
+p1="Make a lot of money "
+p2= "buy now"
+p3= "subscribe this"
+p4="click this"
+
+message=input("Enter comment: ")
+
+if(p1 in message or p2 in message or p3 in message or p4 in message):
+    print("This comment is spam")
+
+else:
+     print("This comment is not a spam")
+
+
+# 4. Write a program to find whether a given username contains less than 10 
+# characters or not. 
+username=input("Enter name :")
+
+if(len(username)>10):
+    print("All is well")
+else:
+    print("Entered username is less than 10 characters.")
+     
+
+# 5. Write a program which finds out whether a given name is present in a list or not.
+list_of_students=["Ayesha","Ali","Zeeshan","Hussnain","Javeria"]
+
+name=input("Enter name :")
+
+if(name in list_of_students):
+    print("The name is in list")
+else:
+    print("The name is not in list.")  
+
+# 6. Write a program to calculate the grade of a student from his marks from the 
+# following scheme: 
+# 90 – 100 => Ex 
+# 80 – 90 => A 
+# 70 – 80 => B 
+# 60 – 70  =>C 
+# 50 – 60 => D 
+# <50 => F
+marks=int(input("Enter marks :"))
+
+if(marks>90):
+    print("Grade is 'Ex'")
+elif(marks>80):
+    print("Grade is 'A'")
+elif(marks>70):
+    print("Grade is 'B'")
+elif(marks>60):
+    print("Grade is 'C'")
+elif(marks>50):
+    print("Grade is 'D'")
+else:
+    print("Grade is 'F'")
+
+
+# 7. Write a program to find out whether a given post is talking about “Harry” or not. 
+
+post=input("Enter post:")
+name=input("Enter name :")
+
+if(name.lower() in post.lower()):
+    print("Found")
+else:
+    print("Not found")   
 
