ba216c328ce134970224e765e6c242387cebc5df
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2026-01-12 09:56:45 -0800

    queue structure

--- DSA_using_Python/Queues_Data_Structure/linear_queue.py
+++ DSA_using_Python/Queues_Data_Structure/linear_queue.py
@@ -1,1 +1,37 @@
 
+class queue:
+    def __init__ (self):
+        self.f = -1
+        self.b = -1
+        self.qu = []
+        self.size = 3
+
+    def insert(self):
+       
+        
+        for i in range (0,3):
+             if self.b == self.size-1 :
+               
+               print (" Queue is full . ")
+               return
+             else:
+               v=input("Enter value : ")
+               self.qu.append(v)
+               self.b += 1
+
+               if self.f ==-1:
+                   self.f = 0
+
+    def display(self):
+        for i in range (self.f,self.b+11):
+            print(self.qu[i])               
+
+
+
+
+q=queue()
+q.insert()
+q.display()
+
+
+
