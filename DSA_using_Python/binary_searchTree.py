37ba74eb0863c374d1cb183885eaf8a49bd47d29
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2026-01-15 20:21:38 -0800

    Pandas

---
+++ DSA_using_Python/binary_searchTree.py
@@ -0,0 +1,32 @@
+class Node:
+
+    def __init__ (self,n,right,left):
+        self.n = n
+        self.right = None
+        self.left = None
+    
+
+
+class BinarySearchTree :
+
+    def __init__(self):
+        self.s = None
+
+    def gotoxy(row, col):
+        print(f"\033[{row};{col}H", end="")    
+
+    def create (self) :
+        row=1
+        col=40
+       
+
+        if(self.s == None) :
+             self.s = Node()
+             self.s.n = data 
+             gotoxy(row,col)
+             print()
+
+        else
+                 
+
+                 
